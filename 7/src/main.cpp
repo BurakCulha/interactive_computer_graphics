1fe043db8bd1121420ae1f838bdcf7ff0f6071f8
burak <burakculha129@gmail.com>
2025-05-15 13:43:31 +0200

    deleted commented lines.

--- 7/src/main.cpp
+++ 7/src/main.cpp
@@ -37,7 +37,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
 
-    GLFWwindow* windowPtr = glfwCreateWindow(600, 600, "render to texture", nullptr, nullptr);
+    GLFWwindow* windowPtr = glfwCreateWindow(600, 600, "tessellation and displacement mapping", nullptr, nullptr);
     if(!windowPtr) {
         cout << "failed to create glfw window. " << endl;
         return -2;
@@ -72,28 +72,12 @@ int main() {
     quadDisplacerPtr = &quadDisplacer;
     quadDisplacer.useProgram();
 
-    // ShaderProgram quadTriangulator("../glsl/quadTriangulator.vert", "../glsl/quadTriangulator.frag");
-    // quadTriangulator.createShader("../glsl/quadTriangulator.tesc", ShaderProgram::shaderTypes::tesControlShader);
-    // quadTriangulator.createShader("../glsl/quadTriangulator.tese", ShaderProgram::shaderTypes::tesEvaluationShader);
-    // quadTriangulator.createShader("../glsl/quadTriangulator.geom", ShaderProgram::shaderTypes::geometryShader);
-    // quadTriangulator.linkShaderObject();
-    // quadTriangulatorPtr = &quadTriangulator;
-
-    
-    // ShaderProgram quadShadow;
 
     Mesh quadMesh("../objFiles/quad.obj");
     quadMesh.myTextureLoader("../img/teapot_disp.png", "displacementMapTexture");
     quadMesh.myTextureLoader("../img/teapot_normal.png", "normalMapTexture.png");
     quadMesh.setPatchSize(4);
 
-    // quadDisplacer.setMat4("mv", glm::mat4(1));
-    // quadDisplacer.setMat4("mvp", glm::mat4(1));
-
-    // vector<glm::vec3> poss = quadMesh.getUniqueVertexPositions();
-    // for(int i=0; i<poss.size(); i++) {
-    //     cout << i <<". vertex: " << glm::to_string(poss[i]) << endl;
-    // }
 
     Camera camera(glm::vec3(0, 0.0, 1.4), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
     glm::mat4 view = camera.getLookAtMatrix();
@@ -158,75 +142,6 @@ int main() {
     quadShadow.useProgram();
     quadShadow.setMat4("mlp", quadShadowProjectionMatrix * shadowLightCamera.getLookAtMatrix());
 
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-    // ShaderProgram teapotShader("../glsl/teapotVS.vert", "../glsl/teapotFS.frag");
-    // teapotShader.useProgram();
-    // teapotShaderPtr = &teapotShader;
-
-    // // Mesh teapotMesh("../objFiles/sphere.obj");
-    // Mesh teapotMesh("../objFiles/teapot.obj");
-    
-    // Camera camera(glm::vec3(0, 2.5, 5.4), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-    // glm::mat4 view = camera.getLookAtMatrix();
-    // glm::mat4 proj = perspectiveProjection_constNear(fovY_scene, 1.0f, -1.0f, -1000.0f);
-
-    // teapotShader.setMat4("projection", proj);
-    // teapotShader.setMat4("view", view);
-    // teapotShader.setMat4("model", teapotMesh.getModelMatrix());
-    // teapotShader.setMat4("transformation_in_camera_frame", transformation_in_camera_frame);
-
-    // Mesh planeMesh("../objFiles/plane_2.obj");
-    // planeMesh.transformMesh(glm::rotate(glm::mat4(1), glm::radians(-90.0f), glm::vec3(1, 0, 0)));
-    // planeMesh.transformMesh(glm::scale(glm::mat4(1), glm::vec3(2.0f)));
-
-    // //////////////////////////////////////////
-
-    // ShaderProgram shadowShader("../glsl/shadow.vert", "../glsl/shadow.frag");
-    // shadowShader.useProgram();
-    // shadowShaderPtr = &shadowShader;
-    // Camera shadowLight(glm::vec3(0, 1.75, -2.4), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-    // shadowShader.setMat4("model", teapotMesh.getModelMatrix());
-    // shadowShader.setMat4("light", shadowLight.getLookAtMatrix());
-    // glm::mat4 shadowProjMatrix = perspectiveProjection_constNear(fovY_shadowLight, 1, -1, -1000);
-    // shadowShader.setMat4("projection", shadowProjMatrix);
-
-
-
-    // Framebuffer fb(600, 600);
-
-    // fb.createDepthTexture("shadowMap");
-    // fb.configureFramebufferForDepthMap();
-
-
-    // teapotShader.useProgram();
-    // teapotShader.setInt(fb.getTexture().type, fb.getTexture().textureUnit);
-    // glm::mat4 shadowMatrix = glm::translate(glm::mat4(1), glm::vec3(0.5, 0.5, 0.5 - 0.001284)) * glm::scale(glm::mat4(1), glm::vec3(0.5)) * proj * shadowLight.getLookAtMatrix() * teapotMesh.getModelMatrix();
-    // teapotShader.setMat4("shadowMatrix", shadowMatrix);
-
-    // glm::vec3 light_target, light_pos;
-    // light_target = shadowLight.getCamTarg();
-    // light_pos = shadowLight.getCamPos();
-
-    // glm::vec4 res;
-    // res = transformation_in_camera_frame * view * glm::vec4(light_pos, 1);
-    // light_pos = glm::vec3(res);
-    
-    // res = transformation_in_camera_frame * view * glm::vec4(light_target, 1);
-    // light_target = glm::vec3(res);
-
-    // teapotShader.setVec3("light_target", light_target);
-    // teapotShader.setVec3("light_position", light_pos);
-
-
-    // Mesh lightMesh("../objFiles/light.obj");
-    // glm::mat4 lightOrienterMatrix(1); // this matrix is used to locate and orient the light model.
-    // lightOrienterMatrix = shadowLight.getLookAtMatrix();
-    // lightOrienterMatrix = glm::inverse(lightOrienterMatrix);
-
 
 
 
@@ -234,49 +149,6 @@ int main() {
 
     while(!glfwWindowShouldClose(windowPtr)) {
 
-
-
-        //////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////
-        // fb.setAsRenderTarget();
-        // glClearColor(0, 0, 0, 0); // shadowMap background color.
-        // glClear(GL_DEPTH_BUFFER_BIT);
-        
-        // // // drawing should be here.
-
-        // shadowShader.useProgram();
-
-        // // teapotMesh.draw(shadowShader);
-
-        // // no need to generate mipmap. but generating for suppressing renderdoc warning.
-        // glGenerateTextureMipmap(fb.getTexture().id); // generate mipmap for rendered texture.
-
-        // glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-        // glViewport(0, 0, 600, 600);
-        // glClearColor(0, 0, 0, 1); // plane background color;
-        // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
-
-        // teapotShader.useProgram();
-        // teapotShader.setMat4("model", teapotMesh.getModelMatrix());
-        // teapotShader.setVec3("diffuse_color", glm::vec3(1, 0, 0));
-        // // teapotMesh.draw(teapotShader);
-
-        // teapotShader.setMat4("model", planeMesh.getModelMatrix());
-        // teapotShader.setVec3("diffuse_color", glm::vec3(0, 1, 0));
-        // // planeMesh.draw(teapotShader);
-
-
-        // teapotShader.setMat4("model", lightOrienterMatrix);
-        // teapotShader.setVec3("diffuse_color", glm::vec3(0, 0, 3));
-        // // lightMesh.draw(teapotShader);
-       
-
-
-        ///////////
 
         // burda quad shadow rendering yapilir.
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -308,10 +180,6 @@ int main() {
         shadowLightMesh.draw(cameraShader);
 
 
-
-
-
-
         ///////////////////////// imgui things begin. ////////////////////////
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
@@ -325,18 +193,13 @@ int main() {
         ImGui::SliderFloat3("specular color: ", &specular_color.x, 0, 1);
         quadDisplacer.useProgram();
         quadDisplacer.setVec3("specular_color", specular_color);
-        // teapotShader.setVec3("specular_color", specular_color);
-
-
-        // static glm::vec3 lightObjPosition = shadowLight.getCamPos();
+    
         static glm::vec3 lightObjPosition(1);
         static float sphericalAlpha{}, sphericalBeta{}, lightDistance{1};
         ImGui::SliderFloat("light spherical Alpha", &sphericalAlpha, -80, 80);
         ImGui::SliderFloat("light spherical Beta", &sphericalBeta, -78, 78);
         ImGui::SliderFloat("light object distance", &lightDistance, 1, 3.2);
-        // lightObjPosition.y = 4 * glm::sin(glm::radians(sphericalAlpha));
-        // lightObjPosition.z = 4 * glm::cos(glm::radians(sphericalAlpha)) * glm::cos(glm::radians(sphericalBeta));
-        // lightObjPosition.x = 4 * glm::cos(glm::radians(sphericalAlpha)) * glm::sin(glm::radians(sphericalBeta));
+
         lightObjPosition.y = lightDistance * glm::sin(glm::radians(sphericalAlpha));
         lightObjPosition.z = lightDistance * glm::cos(glm::radians(sphericalAlpha)) * glm::cos(glm::radians(sphericalBeta));
         lightObjPosition.x = lightDistance * glm::cos(glm::radians(sphericalAlpha)) * glm::sin(glm::radians(sphericalBeta));
@@ -345,35 +208,21 @@ int main() {
         shadowLightCamera.changeProperties(shadowLightPosition, shadowLightTarget, shadowLightUp);
         shadowLightMeshOrienterMatrix = glm::inverse(shadowLightCamera.getLookAtMatrix());
  
-        // shadowLight.changeProperties(lightObjPosition, shadowLight.getCamTarg(), shadowLight.getCamUp());
-        // lightOrienterMatrix = glm::inverse(shadowLight.getLookAtMatrix());
-        // ImGui::SliderFloat3("lightpos:", &lightObjPosition.x, -2, 2);
+
 
         static float shininess{25};
         ImGui::SliderFloat("shininess", &shininess, 0, 250);
         quadDisplacer.setFloat("shininess", shininess);
-        // teapotShader.setFloat("shininess", shininess);
-        // glm::vec4 lightObjPositionInViewSpace = transformation_in_camera_frame * view * glm::vec4(lightObjPosition, 1);
+        
         glm::vec4 lightObjPositionInViewSpace = view * glm::vec4(lightObjPosition, 1);
-        // teapotShader.setVec3("light_position", glm::vec3(lightObjPositionInViewSpace));
 
 
         static float bias{};
         ImGui::SliderFloat("bias", &bias, -1.8, 1.8);
-        // static float light_angle_property{45};
         static float light_angle_property{45};
         ImGui::SliderFloat("light angle property", &light_angle_property, 0, 88.0f);
-        // teapotShader.setFloat("light_angle_property", glm::radians(light_angle_property));
         fovY_shadowLight = light_angle_property;
         quadShadowProjectionMatrix = perspectiveProjection_constNear(fovY_shadowLight, 1, -1, -1000);
-
-        // shadowMatrix = glm::translate(glm::mat4(1), glm::vec3(0.5, 0.5, 0.5 + bias/100.0f)) * glm::scale(glm::mat4(1), glm::vec3(0.5)) * shadowProjMatrix * shadowLight.getLookAtMatrix();
-        // teapotShader.setMat4("shadowMatrix", shadowMatrix);
-
-        // shadowShader.useProgram();
-        // shadowShader.setMat4("light", shadowLight.getLookAtMatrix());
-        // shadowProjMatrix = perspectiveProjection_constNear(fovY_shadowLight, 1, -1, -1000);
-        // shadowShader.setMat4("projection", shadowProjMatrix);
 
 
         static glm::ivec4 outerLevels{1}, innerLevels{1};
@@ -415,21 +264,11 @@ int main() {
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         ///////////////////////// imgui things end. ////////////////////////
 
-        //////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////
+
 
         glfwSwapBuffers(windowPtr);
         glfwPollEvents();
     }
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
 
 
 
